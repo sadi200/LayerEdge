62cbdca021aef0a45cafcd4411a647ce0ddb0811
sadi200 <63481667+sadi200@users.noreply.github.com>
2025-02-08 04:26:02 -0600

    Add files via upload

---
+++ Layeredg-main/utils/banner.js
@@ -0,0 +1,13 @@
+const banner = `ＢＩＬＡＬ ＳＴＵＤＩＯ 
+     === Running LayerEdge CLI Version ===
+** ====================================== **
+*    This script is created for free use   *
+*  Do not sell or distribute it for profit *
+** ====================================== **
+
+
+* Author: @BilalStudio2                        
+* Group: https://t.me/Bilalstudio2                
+* Github Link: https://t.me/Bilalstudio2         
+                 `
+export default banner;
